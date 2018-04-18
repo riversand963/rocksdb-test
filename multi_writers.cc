e3668f366ebdc5735d6219d3a6d000938532527e
Yanqin Jin <yanqin@fb.com>
2018-04-17 18:03:03 -0700

    Add new tests.

--- multi_writers.cc
+++ multi_writers.cc
@@ -26,7 +26,6 @@ DEFINE_int32(value_size, 100, "Value size");
 DEFINE_string(db, "", "Use the db with the following name.");
 DEFINE_bool(destroy_db, true, "Destroy existing DB before running the test");
 DEFINE_int32(runtime_sec, 60, "How long are we running for, in seconds");
-DEFINE_int32(seed, 0xdeadbeef, "Random seed");
 
 namespace rocksdb {
 class MultiWritersTest {
@@ -38,40 +37,133 @@ public:
       default_db_path += "/multi_writers";
       FLAGS_db = default_db_path;
     }
-    Options options;
+
+    DBOptions db_options;
+    db_options.create_if_missing = true;
+    //db_options.use_direct_io_for_flush_and_compaction = true;
+    //db_options.bytes_per_sync = 1;
+    db_options.use_fsync = false;
+
+    ColumnFamilyOptions cf_options;
+    Options options(db_options, cf_options);
+
     if (FLAGS_destroy_db) {
       DestroyDB(FLAGS_db, options);
     }
 
-    options.create_if_missing = true;
-    options.write_buffer_size = 256 * 1024;
-
     DB* db;
     Status s = DB::Open(options, FLAGS_db, &db);
     if (!s.ok()) {
+      cerr << "Cannot open database: " << s.ToString() << endl << endl;
+      std::abort();
+    }
+
+    ColumnFamilyHandle *handle;
+    s = db->CreateColumnFamily(cf_options, "cf1", &handle);
+    if (!s.ok()) {
+      cerr << "Cannot create column family: " << s.ToString() << endl << flush;
+      std::abort();
+    }
+    delete db;
+    db = nullptr;
+
+    std::vector<ColumnFamilyDescriptor> cf_descs;
+    cf_descs.push_back(ColumnFamilyDescriptor("default", cf_options));
+    cf_descs.push_back(ColumnFamilyDescriptor("cf1", cf_options));
+
+    std::vector<ColumnFamilyHandle*> handles;
+    s = DB::Open(options, FLAGS_db, cf_descs, &handles, &db);
+    if (!s.ok()) {
       cerr << "Cannot open database: " << s.ToString() << endl << flush;
       std::abort();
+    }
+    for (auto h : handles) {
+      handles_.push_back(h);
     }
     db_.reset(db);
   }
 
   void WriteWithWALThreadFunc() {
+    WriteOptions write_options;
+    write_options.sync = false;
+    write_options.disableWAL = false;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(1, 10000);
+    FlushOptions flush_options;
+    //flush_options.wait = true;
     while (!stop_.load(std::memory_order_relaxed)) {
-      writes_with_wal_++;
+      int r = dist(gen);
+      std::string temp = std::to_string(r);
+      Slice key(temp);
+      Slice value(temp + "_value");
+      Status s = db_->Put(write_options, handles_[0], key, value);
+      if (s.ok()) {
+        writes_with_wal_++;
+      }
+      s = db_->Flush(flush_options, handles_[0]);
+      if (!s.ok()) {
+        cerr << "Cannot flush memtable: " << s.ToString() << endl << flush;
+        std::abort();
+      }
     }
   }
 
   void WriteWithoutWALThreadFunc() {
+    WriteOptions write_options;
+    write_options.sync = false;
+    write_options.disableWAL = true;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(1, 10000);
     while (!stop_.load(std::memory_order_relaxed)) {
-      writes_without_wal_++;
+      int r = dist(gen);
+      std::string temp = std::to_string(r);
+      Slice key(temp);
+      Slice value(temp + "_vallue");
+      Status s = db_->Put(write_options, handles_[1], key, value);
+      if (s.ok()) {
+        writes_without_wal_++;
+      } else {
+        cerr << "Cannot Put: " << s.ToString() << endl << flush;
+        std::abort();
+      }
+    }
+  }
+
+  void MultiGetThreadFunc() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(1, 10000);
+    ReadOptions read_options;
+    while (!stop_.load(std::memory_order_relaxed)) {
+      /*
+      int r = dist(gen);
+      std::string temp = std::to_string(r);
+      std::vector<Slice> keys;
+      keys.push_back(Slice(temp));
+      std::vector<std::string> values;
+      std::vector<ColumnFamilyHandle*> cfs;
+      cfs.push_back(handles_[1]);
+      std::vector<Status> status_vec = db_->MultiGet(read_options, cfs, keys, &values);
+      for (auto s : status_vec) {
+        if (!s.ok() && !s.IsNotFound()) {
+          cerr << "MultiGet failed: " << s.ToString() << endl << flush;
+          std::abort();
+        }
+      }
+      */
+      Options options = db_->GetOptions(handles_[1]);
+      multigets_++;
     }
   }
 
   int Run() {
     writes_with_wal_.store(0, std::memory_order_relaxed);
     writes_without_wal_.store(0, std::memory_order_relaxed);
+    multigets_.store(0, std::memory_order_relaxed);
     threads_.emplace_back([&]() { WriteWithWALThreadFunc(); });
-    threads_.emplace_back([&]() { WriteWithoutWALThreadFunc(); });
+    threads_.emplace_back([&]() { MultiGetThreadFunc(); });
 
     Env::Default()->SleepForMicroseconds(FLAGS_runtime_sec * 1000 * 1000);
 
@@ -80,7 +172,7 @@ public:
       t.join();
     }
     threads_.clear();
-    cout << writes_with_wal_ << " " << writes_without_wal_ << endl << flush;
+    cout << writes_with_wal_ << " " << writes_without_wal_ << " " << multigets_ << " " << lock_acquired_ << endl << flush;
     return 0;
   }
 
@@ -88,8 +180,11 @@ private:
   std::atomic<bool> stop_;
   std::vector<std::thread> threads_;
   std::unique_ptr<DB> db_;
+  std::vector<ColumnFamilyHandle*> handles_;
   std::atomic<long> writes_with_wal_;
   std::atomic<long> writes_without_wal_;
+  std::atomic<long> multigets_;
+  long lock_acquired_ = 0;
 };
 } /* namespace rocksdb */
 
